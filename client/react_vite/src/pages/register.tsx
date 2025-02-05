8ca437cd41a3269d220dd4ab34164911f6a33844
LaalithaIndrajith <laalithagajanayake@gmail.com>
2025-02-05 09:28:47 +0530

    feat(frontend): Implement client-side validation for the Register User form using Zod and react-form-hook

--- client/react_vite/src/pages/register.tsx
+++ client/react_vite/src/pages/register.tsx
@@ -3,8 +3,42 @@ import {Label} from "@/components/ui/label.tsx";
 import {Input} from "@/components/ui/input.tsx";
 import {Button} from "@/components/ui/button.tsx";
 import {Link} from "react-router";
+import {z} from "zod";
+import {useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
+import {toast} from "@/hooks/use-toast.ts";
+
+const registerFormSchema = z.object({
+    email: z.string().email({message: "Please enter a valid email"}),
+    password: z.string().min(1, { message: 'Password is Required' }),
+    confirmPassword: z
+        .string()
+        .min(1, { message: 'Please confirm your password' }),
+}).superRefine((val, ctx) => {
+    if (val.password !== val.confirmPassword) {
+        ctx.addIssue({
+            code: z.ZodIssueCode.custom,
+            message: 'Password is not the same as confirm password',
+            path: ['confirmPassword'],
+        })
+    }
+})
 
 export  function RegisterPage(){
+    const form= useForm<z.infer<typeof registerFormSchema>>({
+        resolver: zodResolver(registerFormSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+            confirmPassword: "",
+        },
+    })
+
+    function onSubmit(values: z.infer<typeof registerFormSchema>) {
+        console.log(values);
+    }
+
     return (
         <>
             <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
@@ -17,42 +51,66 @@ export  function RegisterPage(){
                             </CardDescription>
                         </CardHeader>
                         <CardContent>
-                            <form>
-                                <div className="flex flex-col gap-6">
-                                    <div className="grid gap-2">
-                                        <div className="flex items-center">
-                                            <Label htmlFor="password">Email</Label>
+                            <Form {...form}>
+                                <form onSubmit={form.handleSubmit(onSubmit)}>
+                                    <div className="flex flex-col gap-5">
+                                        <div className="grid gap-2">
+                                            <FormField
+                                                control={form.control}
+                                                name="email"
+                                                render={({ field }) => (
+                                                    <FormItem>
+                                                        <FormLabel>Email</FormLabel>
+                                                        <FormControl>
+                                                            <Input style={{marginTop:4}} placeholder="john@example.com" {...field} />
+                                                        </FormControl>
+                                                        <FormMessage />
+                                                    </FormItem>
+                                                )}
+                                            />
                                         </div>
-                                        <Input
-                                            id="email"
-                                            type="email"
-                                            placeholder="m@example.com"
-                                            required
-                                        />
-                                    </div>
-                                    <div className="grid gap-2">
-                                        <div className="flex items-center">
-                                            <Label htmlFor="password">Password</Label>
+                                        <div className="grid gap-2">
+                                            <FormField
+                                                control={form.control}
+                                                name="password"
+                                                render={({ field }) => (
+                                                    <FormItem>
+                                                        <FormLabel>Password</FormLabel>
+                                                        <FormControl>
+                                                            <Input style={{marginTop:4}} type="password" {...field} />
+                                                        </FormControl>
+                                                        <FormMessage />
+                                                    </FormItem>
+                                                )}
+                                            />
                                         </div>
-                                        <Input id="password" type="password" required />
-                                    </div>
-                                    <div className="grid gap-2">
-                                        <div className="flex items-center">
-                                            <Label htmlFor="password">Confirm Password</Label>
+                                        <div className="grid gap-2">
+                                            <FormField
+                                                control={form.control}
+                                                name="confirmPassword"
+                                                render={({ field }) => (
+                                                    <FormItem>
+                                                        <FormLabel>Confirm Password</FormLabel>
+                                                        <FormControl>
+                                                            <Input style={{marginTop:4}} type="password" {...field} />
+                                                        </FormControl>
+                                                        <FormMessage />
+                                                    </FormItem>
+                                                )}
+                                            />
                                         </div>
-                                        <Input id="password" type="password" required />
+                                        <Button type="submit" className="w-full">
+                                            Sign up
+                                        </Button>
                                     </div>
-                                    <Button type="submit" className="w-full">
-                                        Sign up
-                                    </Button>
-                                </div>
-                                <div className="mt-4 text-center text-sm">
-                                    Already have an account?{" "}
-                                    <Link to={'/'} className="underline underline-offset-4">
-                                        Log in
-                                    </Link>
-                                </div>
-                            </form>
+                                    <div className="mt-4 text-center text-sm">
+                                        Already have an account?{" "}
+                                        <Link to={'/'} className="underline underline-offset-4">
+                                            Log in
+                                        </Link>
+                                    </div>
+                                </form>
+                            </Form>
                         </CardContent>
                     </Card>
                 </div>
