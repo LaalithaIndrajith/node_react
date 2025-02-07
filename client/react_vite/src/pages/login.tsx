6d61a79a0488fb530e23cc0bfb6dfabbdb870f4f
LaalithaIndrajith <laalithagajanayake@gmail.com>
2025-02-07 09:26:19 +0530

    feat(frontend): Implement user authentication with local storage

--- client/react_vite/src/pages/login.tsx
+++ client/react_vite/src/pages/login.tsx
@@ -1,10 +1,82 @@
 import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card.tsx";
-import {Label} from "@/components/ui/label.tsx";
 import {Input} from "@/components/ui/input.tsx";
 import {Button} from "@/components/ui/button.tsx";
-import {Link} from "react-router";
+import {Link, useNavigate} from "react-router";
+import {z} from "zod";
+import {useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form.tsx";
+import axios from "axios";
+import AlertType from "@/constants/alert-type.ts";
+import {PopupAlert} from "@/components/common/popup-alert.tsx";
+import {useState} from "react";
 
-export  function LoginPage(){
+const loginFormSchema = z.object({
+    email: z.string().email({message: "Please enter a valid email"}),
+    password: z.string({ message: 'Password is Required' }),
+})
+
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+
+export function LoginPage(){
+    const [alert, setAlert] = useState<{ alertType: AlertType; titleText: string; description: string; isOpen: boolean, navigateTo?:string }>({
+        alertType: AlertType.success,
+        titleText: "",
+        description: "",
+        isOpen: false,
+    });
+    const navigate = useNavigate();
+    const loginForm= useForm<z.infer<typeof loginFormSchema>>({
+        resolver: zodResolver(loginFormSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+    })
+
+    async function onSubmit(values: z.infer<typeof loginFormSchema>) {
+        console.log(values);
+        try{
+            const authentication = await axios.post(`${BACKEND_URL}/auth/login`,{
+                    email: values.email,
+                    password: values.password,
+
+                }, {
+                    headers: {
+                        'Content-Type': 'application/json',
+                    }
+                }
+            )
+            console.log(`✅ User authenticated successfully: ${JSON.stringify(authentication.data)}`);
+            localStorage.setItem("authToken", authentication.data.authentication.sessionToken);
+            const userDetails =   {
+                "username": authentication.data.username,
+                "email": authentication.data.email,
+            }
+            localStorage.setItem("userDetails", JSON.stringify(userDetails));
+            navigate("/home");
+        }catch (err){
+            if (axios.isAxiosError(err)) {
+                console.error('❌ Error occurred:', err.response?.data.error);
+                setAlert({
+                    alertType: AlertType.success,
+                    titleText: "User Login",
+                    description: err.response?.data.error,
+                    isOpen: true,
+                });
+            } else {
+                console.error('❌ Unknown error occurred:', err);
+                setAlert({
+                    alertType: AlertType.warning,
+                    titleText: "User Registration Unsuccessful",
+                    description: "Unknown error occurred. Please try again",
+                    isOpen: true,
+                });
+            }
+        }
+
+    }
+
     return (
         <>
             <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
@@ -17,24 +89,38 @@ export  function LoginPage(){
                             </CardDescription>
                         </CardHeader>
                         <CardContent>
-                            <form>
+                            <Form {...loginForm}>
+                                <form onSubmit={loginForm.handleSubmit(onSubmit)}>
                                 <div className="flex flex-col gap-6">
                                     <div className="grid gap-2">
-                                        <div className="flex items-center">
-                                            <Label htmlFor="password">Email</Label>
-                                        </div>
-                                        <Input
-                                            id="email"
-                                            type="email"
-                                            placeholder="m@example.com"
-                                            required
+                                        <FormField
+                                            control={loginForm.control}
+                                            name="email"
+                                            render={({ field }) => (
+                                                <FormItem>
+                                                    <FormLabel>Email</FormLabel>
+                                                    <FormControl>
+                                                        <Input style={{marginTop:4}} placeholder="john@example.com" {...field} />
+                                                    </FormControl>
+                                                    <FormMessage />
+                                                </FormItem>
+                                            )}
                                         />
                                     </div>
                                     <div className="grid gap-2">
-                                        <div className="flex items-center">
-                                            <Label htmlFor="password">Password</Label>
-                                        </div>
-                                        <Input id="password" type="password" required />
+                                        <FormField
+                                            control={loginForm.control}
+                                            name="password"
+                                            render={({ field }) => (
+                                                <FormItem>
+                                                    <FormLabel>Confirm Password</FormLabel>
+                                                    <FormControl>
+                                                        <Input style={{marginTop:4}} type="password" {...field} />
+                                                    </FormControl>
+                                                    <FormMessage />
+                                                </FormItem>
+                                            )}
+                                        />
                                     </div>
                                     <Button type="submit" className="w-full">
                                         Login
@@ -47,10 +133,19 @@ export  function LoginPage(){
                                     </Link>
                                 </div>
                             </form>
+                            </Form>
                         </CardContent>
                     </Card>
                 </div>
             </div>
+            <PopupAlert
+                alertType={alert.alertType}
+                titleText={alert.titleText}
+                description={alert.description}
+                isOpen={alert.isOpen}
+                onClose={() => setAlert({ ...alert, isOpen: false })}
+                navigateTo={alert.navigateTo}
+            />
         </>
     )
 }
