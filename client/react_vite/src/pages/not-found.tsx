453a62e74daf30e92757a6ac70a019fd77bc31f1
LaalithaIndrajith <laalithagajanayake@gmail.com>
2025-02-21 01:30:23 +0530

    feat(frontend): Update the NotFound page

--- client/react_vite/src/pages/not-found.tsx
+++ client/react_vite/src/pages/not-found.tsx
@@ -1,6 +1,8 @@
 import {Link} from "react-router";
+import {AuthHelper} from "@/helpers/auth-helper.ts";
 
 export default function NotFoundPage() {
+    const isAuthenticated = AuthHelper.isAuthenticated();
     return (
         <div className="flex items-center min-h-screen px-4 py-12 sm:px-6 md:px-8 lg:px-12 xl:px-16">
             <div className="w-full space-y-6 text-center">
@@ -12,7 +14,7 @@ export default function NotFoundPage() {
                     to={'/'}
                     className="inline-flex h-10 items-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
                 >
-                    Return to Home page
+                    {(isAuthenticated) ? 'Return to Home page' : 'Return to Login'}
                 </Link>
             </div>
         </div>
